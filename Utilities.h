58e07b217c0546a2632ca07e84c3f7b9debe266c
ChanhChumChim <22028052@vnu.edu.vn>
2023-04-09 16:30:24 +0700

    'init'

---
+++ Utilities.h
@@ -0,0 +1,17 @@
+#ifndef UTILITIES__H
+#define UTILITIES__H
+
+#include <iostream>
+#include <SDL.h>
+
+using namespace std;
+
+void initSDL(SDL_Window*& window, SDL_Renderer*& renderer, int SCREEN_WIDTH, int SCREEN_HEIGHT, const string &WINDOW_TITLE);
+
+void logSDLError(std::ostream& os, const std::string& msg, bool fatal = false);
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+void waitUntilKeyPressed();
+
+#endif 
